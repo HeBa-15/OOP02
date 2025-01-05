8ac2391bffc8c34e93abc4d5509e7ff67133cdcc
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-05 21:59:41 +0200

    Add project files.

---
+++ OOP02/Program.cs
@@ -0,0 +1,24 @@
+﻿using OOP02.Encapsulation;
+
+namespace OOP02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Encapsulation
+
+            Employee employee = new Employee(1001, "Ahmed Nasr", 10_000);
+            
+            employee.SetId(2002); //Set ID
+
+            employee.Name = "Ibrahim Ali";
+            Console.WriteLine(employee.Name);
+            Console.WriteLine(employee.GetId()); // get Id
+
+            Console.WriteLine(employee);
+            #endregion
+
+        }
+    }
+}
