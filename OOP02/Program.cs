a85127ff3574296e57eb1a53bb42814e2fc018d7
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-06 00:46:51 +0200

    Indexer

--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -19,6 +19,24 @@ namespace OOP02
             Console.WriteLine(employee);
             #endregion
 
+
+            #region PhoneBook
+            PhoneBook note = new PhoneBook(10);
+            note.AddPerson(0, "Ahmed", 111);
+            note.AddPerson(1, "Nadia", 222);
+            note.AddPerson(2, "Osama", 333);
+
+            note.SetNumber("Ahmed", 999);
+            Console.WriteLine(note.GetPersonNumber("Ahmed"));
+
+            for(int i = 0; i < note.Size; i++)
+            {
+                Console.WriteLine(note[i]);
+            }
+            Console.WriteLine(note.ToString());
+            #endregion
+
+
         }
     }
 }
